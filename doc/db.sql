05e448d5dbdebdf79c5a1b4de5d5c5de911e0564
Suxto <suxton@icloud.com>
2023-07-14 00:13:14 +0800

    Update db.sql

--- doc/db.sql
+++ doc/db.sql
@@ -1,5 +1,67 @@
 CREATE DATABASE KunMall;
 USE KunMall;
-CREATE TABLE Users(
+CREATE TABLE Users
+(
+    id      INT PRIMARY KEY AUTO_INCREMENT,
+    isAdmin BOOLEAN DEFAULT FALSE,
+    passwd  VARCHAR(40) NOT NULL
 
 );
+
+CREATE TABLE Orders
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    userID    INT NOT NULL REFERENCES Users (id),
+    CPUID     INT NOT NULL REFERENCES CPU (id),
+    GPUID     INT NOT NULL REFERENCES GPU (id),
+    DriveID   INT NOT NULL REFERENCES Drive (id),
+    MemoryID  INT NOT NULL REFERENCES Memory (id),
+    address   VARCHAR(40),
+    money     DECIMAL(10, 2),
+    orderTime DATETIME,
+    status    BOOLEAN,
+    comment   VARCHAR(70)
+);
+
+CREATE TABLE Recommends
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    CPUID    INT NOT NULL REFERENCES CPU (id),
+    GPUID    INT NOT NULL REFERENCES GPU (id),
+    DriveID  INT NOT NULL REFERENCES Drive (id),
+    MemoryID INT NOT NULL REFERENCES Memory (id),
+    comment   VARCHAR(70)
+);
+
+CREATE TABLE CPU
+(
+    id     INT PRIMARY KEY AUTO_INCREMENT,
+    money  DECIMAL(10, 2) NOT NULL,
+    name   VARCHAR(20),
+    amount INT
+);
+
+CREATE TABLE GPU
+(
+    id     INT PRIMARY KEY AUTO_INCREMENT,
+    money  DECIMAL(10, 2) NOT NULL,
+    name   VARCHAR(20),
+    amount INT
+);
+
+CREATE TABLE Drive
+(
+    id     INT PRIMARY KEY AUTO_INCREMENT,
+    money  DECIMAL(10, 2) NOT NULL,
+    name   VARCHAR(20),
+    amount INT
+);
+
+CREATE TABLE Memory
+(
+    id     INT PRIMARY KEY AUTO_INCREMENT,
+    money  DECIMAL(10, 2) NOT NULL,
+    name   VARCHAR(20),
+    amount INT
+);
+
