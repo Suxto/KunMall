146571610c116e87c708cc4b44a625e9dd182164
Suxto <suxton@icloud.com>
2023-07-13 20:43:42 +0800

    init

---
+++ src/main/java/com/suxton/kunmall/KunMallApplication.java
@@ -0,0 +1,13 @@
+package com.suxton.kunmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunMallApplication.class, args);
+    }
+
+}
